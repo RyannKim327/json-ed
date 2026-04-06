abd063fb0a1211edb27f4ed67c15348e3f7859ce
RyannKim327 <rksesgundo123@gmail.com>
2026-04-06 23:42:54 +0800

    04-06-26 23:42 [fix: sanitize]

--- src/middlewares/sanitize.ts
+++ src/middlewares/sanitize.ts
@@ -1,24 +1,24 @@
 import { data_structure, main_structure } from "../interface";
 
-export default function sanitizingData(table: string, data: data_structure, cache: main_structure) {
+export default function sanitizingData(table: string, data: data_structure, cache: main_structure): data_structure | { error: string } {
 	const allowedColumns = cache["table_struct"][table]
 
 	// TODO: Key filteration
-	if (
-		Array.isArray(allowedColumns) &&
-		Object.keys(data).every(col => allowedColumns.includes(col.toLowerCase()))
-	) {
+	Object.keys(data).map((key: string) => {
+		if (Array.isArray(allowedColumns)) {
+			if (!allowedColumns.includes(key.toLowerCase())) {
+				delete data[key]
+			}
+		}
+	})
 
-		const temp: data_structure = data
-		data = {}
+	const temp: data_structure = data
+	data = {}
+	if (Array.isArray(allowedColumns)) {
 		allowedColumns.forEach((column: string) => {
 			data[column.toLowerCase()] = temp[column.toLowerCase()] ?? null
 		})
-
-		return data
-	} else {
-		return {
-			"error": "error"
-		}
 	}
+
+	return data
 }
