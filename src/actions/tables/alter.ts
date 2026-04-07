962fee114996a18f87e2095f43d4239f9b2ed990
RyannKim327 <rksesgundo123@gmail.com>
2026-04-07 09:28:02 +0800

    04-07-26  9:28 [chore: alter initially started]

--- src/actions/tables/alter.ts
+++ src/actions/tables/alter.ts
@@ -4,9 +4,34 @@
  */
 
 import { main_structure } from "../../interface";
+import { save } from "../../middlewares/data_control";
+import { c } from "../../utils";
 
 export default function alter(filename: string, key: string, cache: main_structure) {
 	return (table: string, newCol?: string[], deleteCol?: string[]) => {
 		// TODO: Development soon, but I already have an idea, I need to figure it out first
+		table = table.toLowerCase()
+		const reservedTable = "table_struct"
+
+		if (table === "table_struct") {
+			throw new Error("Cannot access reserved table: table_struct");
+		}
+
+		if (deleteCol !== undefined) {
+			deleteCol.map((col: string, i: number) => {
+				if (Array.isArray(cache[reservedTable][table])) {
+					if (cache[reservedTable][table][i] !== col) {
+						cache[reservedTable][table] = cache[reservedTable][table].splice(i, 1)
+					}
+				}
+			})
+		}
+		if (newCol !== undefined) {
+			if (Array.isArray(cache[reservedTable][table])) {
+				cache[reservedTable][table] = [...cache[reservedTable][table], ...newCol]
+			}
+		}
+		save(filename, key, cache)
+		return cache
 	}
 }
